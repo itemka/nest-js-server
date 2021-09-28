9c14a216ea22ee30bb152ea63ee0a7445b653d36
itemka <itemka2503@gmail.com>
2021-09-29 01:53:23 +0300

    - add dotenv - use docker to up the mysql - connect mysql into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
+import * as path from 'path'
+import * as dotenv from 'dotenv'
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { EventsController } from './events.controller';
 import { AppService } from './app.service';
 
+dotenv.config({
+  path: path.resolve(__dirname, '../.env')
+})
+
+const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USERNAME,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+} = process.env
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: MYSQL_HOST,
+      port: Number(MYSQL_PORT),
+      username: MYSQL_USERNAME,
+      password: MYSQL_PASSWORD,
+      database: MYSQL_DATABASE,
+    })
+  ],
   controllers: [
     AppController,
     EventsController,
