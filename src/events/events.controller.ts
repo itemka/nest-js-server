72f4885099ef6d5def5c99dd68fb6458487b62f2
itemka <itemka2503@gmail.com>
2021-10-10 18:05:18 +0300

    practice the find query

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -8,11 +8,15 @@ import {
   Body,
   HttpCode,
 } from '@nestjs/common'
+import {
+  Like,
+  MoreThan,
+  Repository,
+} from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateEventDto } from './dto/create-event.dto'
 import { UpdateEventDto } from './dto/update-event.dto'
 import { Event } from './events.entity'
-import { Repository } from 'typeorm';
-import { InjectRepository } from '@nestjs/typeorm';
 
 @Controller({ path: '/events' })
 export class EventsController {
@@ -24,6 +28,22 @@ export class EventsController {
   @Get()
   async findAll() {
     return await this.repository.find()
+  }
+
+  @Get('/practice_find_query')
+  async practice() {
+    return await this.repository.find({
+      select: ['id', 'when'],
+      where: [
+        {
+          id: MoreThan(3),
+          when: MoreThan(new Date('2021-02-12T13:00:00.000Z'))
+        },
+        { description: Like('%meet%')}
+      ],
+      take: 2,
+      order: { id: 'DESC' }
+    })
   }
 
   @Get(':id')
